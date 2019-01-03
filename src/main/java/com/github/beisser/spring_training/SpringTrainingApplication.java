9eb4a31b4525c1fbc2631365ebade191562d6701
beisser <ryker@hotmail.de>
2019-01-03 13:49:56 +0100

    init

---
+++ src/main/java/com/github/beisser/spring_training/SpringTrainingApplication.java
@@ -0,0 +1,14 @@
+package com.github.beisser.spring_training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrainingApplication.class, args);
+    }
+
+}
+
